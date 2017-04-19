71a7d6c4a90af7da785a272b65dcc8e3659922ec
joan.feng <joan.feng@pictureworks.biz>
2017-04-19 10:18:12 +0800

    fix

--- routes/CI.js
+++ routes/CI.js
@@ -13,17 +13,18 @@ const ok = require('../models/server/throwCode').ok;
 router.post('/dev/builder', function(req, res, next) {
 	let userName = req.body.user_name;
 	let commitId = req.body.after;
-	let openAndPull = [];
+	let openAndPull = '';
 	for (let i = 0; i < script.gitPath.length; i++) {
-		openAndPull.push(`cd ${script.gitPath[i]} \n`);
-		openAndPull.push(`git checkout develop \n`);
-		openAndPull.push(`git pull origin develop \n`);
+		openAndPull += `
+		cd ${script.gitPath[i]} \n`;
+		openAndPull += `git checkout develop \n`;
+		openAndPull += `git pull origin develop \n`;
 	}
-	let command = `${scriptPath}${script.restartScript} ${openAndPull}`;
-	console.log(command);
+	let openAndPull += `pm2 reload all\n`;
+	console.log(openAndPull);
 	let execScript = new Promise((resolve, reject) => {
-		exec(command, (err, stdout, stderr) => {
-			console.log(stdout,'======',stderr);
+		exec(openAndPull, (err, stdout, stderr) => {
+			// console.log(err, stdout, stderr);
 			if (err) {
 				return reject(err, stderr);
 			}
@@ -33,7 +34,6 @@ router.post('/dev/builder', function(req, res, next) {
 	execScript.then((result) => {
 			return ok(res, req.originalUrl, req.body);
 		})
-		.then(()=>{})
 		.catch((err) => {
 			return throwErrCode(res, '400', req.originalUrl, err);
 		});
